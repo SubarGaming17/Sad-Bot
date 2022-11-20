712bf220568874bdc6d2f47fa4ea866e08ce8b84
SubarGaming17 <106585460+SubarGaming17@users.noreply.github.com>
2022-11-20 09:13:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -38,12 +38,12 @@ global.thumb = fs.readFileSync("./src/papah2.jpg")
 global.profil = fs.readFileSync("./src/papah.png")
 global.zippy = (await import('./lib/zippy.cjs')).zippy
 global.tiny = (await import('./lib/tiny.cjs')).default
-global.gc = "https://chat.whatsapp.com/KYIzgolOI1I66efv9UMfch"
-global.gh = "https://github.com/FahriAdison"
-global.yt = "https://bit.ly/Papah-Chan"
-global.wm = '                「 ꜱᴀᴅ BOT あ⁩ 」' 
-global.wm2 = '꒷︶꒷꒥꒷ ‧₊˚ ꒰ฅ˘օառɛʀ - chαn˘ฅ ꒱ ‧₊˚꒷︶꒷꒥꒷'
-global.wm3 = '⫹⫺ ꜱᴀᴅ 𝗕𝗢𝗧'
+global.gc = "https://chat.whatsapp.com/GTcVDY465eVJg9XP2VvPKM"
+global.gh = "https://github.com/SubarGaming17"
+global.yt = "https://www.youtube.com/channel/UC8G_J3AE6B40Ejtg_deYNcA"
+global.wm = '                「 SubarBotz あ⁩ 」' 
+global.wm2 = 'Nomor bot:6287755567549'
+global.wm3 = '⫹⫺ SubarBotz'
 function ucapan() {
 	var time = moment.tz('Asia/Jakarta').format('HH')
 	var res
@@ -139,24 +139,24 @@ global.APIKeys = { // APIKey Here
 	'https://ghostui-api.herokuapp.com/': 'OC2eTK'
 }
 // Sticker WM
-global.packname = ''
-global.author = ''
+global.packname = 'SubarBotz'
+global.author = 'Nomor bot:6287755567549'
 
-global.sgc = 'https://chat.whatsapp.com/Eo0rT2MCSiWIijy7fMHlB7'
-global.sig = 'https://instagram.com/bot_whangsaf'
+global.sgc = 'https://chat.whatsapp.com/GTcVDY465eVJg9XP2VvPKM'
+global.sig = 'https://instagram.com/subar_gaming_17'
 //========Url Template Buttons=========//
 global.dtu = 'ɪɴꜱᴛᴀɢʀᴀᴍ'
-global.urlnya = "https://www.instagram.com/bot_whangsaf"
+global.urlnya = "https://www.instagram.com/subar_gaming_17"
 
 //============= callButtons =============//
 global.dtc = 'ᴄᴀʟʟ ᴏᴡɴᴇʀ'
-global.phn = '+62 822-8721-9167'
+global.phn = '+62 858-5657-5305'
 global.multiplier = 69 // The higher, The harder levelup
 /*============== NOMOR ==============*/
-global.nomorbot = '66801944580'
-global.nomorown = '6282287219167'
-global.namebot = 'Sad-Bot'
-global.nameown = 'Papah-Chan'
+global.nomorbot = '6287755567549'
+global.nomorown = '6285856575305'
+global.namebot = 'SubarBotz'
+global.nameown = 'SubarGanzz'
 global.thumb2 = fs.readFileSync('./papah.jpg')
 
 /*=========== FAKE SIZE ===========*/
